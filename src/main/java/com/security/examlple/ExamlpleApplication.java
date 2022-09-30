9c8bd3fdda6771377d4189af9d87822dac43a3f7
mtae616 <mtae616@naver.com>
2022-09-30 17:29:07 +0900

    init project

---
+++ src/main/java/com/security/examlple/ExamlpleApplication.java
@@ -0,0 +1,13 @@
+package com.security.examlple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamlpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamlpleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/security/examlple/ExamlpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.examlple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamlpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
