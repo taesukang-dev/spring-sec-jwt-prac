d33457d5f7c505d3b82096f54813f2abaaabbda6
mtae616 <mtae616@naver.com>
2022-10-01 18:16:09 +0900

    add test api

--- src/main/java/com/sec/prac/controller/MemberController.java
+++ src/main/java/com/sec/prac/controller/MemberController.java
@@ -2,12 +2,12 @@ package com.sec.prac.controller;
 
 import com.sec.prac.dto.request.MemberSignInRequest;
 import com.sec.prac.dto.request.MemberSignUpRequest;
+import com.sec.prac.dto.security.MemberPrincipal;
 import com.sec.prac.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -26,5 +26,12 @@ public class MemberController {
     @PostMapping("/login")
     public String login(@RequestBody MemberSignInRequest memberSignInRequest) {
         return memberService.login(memberSignInRequest);
+    }
+
+    @GetMapping("/test")
+    @PreAuthorize("hasAnyRole('USER')")
+//    @PreAuthorize("hasAnyRole('ADMIN')")
+    public MemberPrincipal testing(@AuthenticationPrincipal MemberPrincipal memberPrincipal) {
+        return memberPrincipal;
     }
 }
