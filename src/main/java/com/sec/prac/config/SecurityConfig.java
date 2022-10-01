44088cfb0bcf16cbeebdd411cbbbbebf2b842ff8
mtae616 <mtae616@naver.com>
2022-10-01 17:48:13 +0900

    add jwt errorhandler

--- src/main/java/com/sec/prac/config/SecurityConfig.java
+++ src/main/java/com/sec/prac/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.sec.prac.config;
 
+import com.sec.prac.jwt.JwtAccessDeniedHandler;
+import com.sec.prac.jwt.JwtAuthenticationEntryPoint;
 import com.sec.prac.jwt.JwtSecurityConfig;
 import com.sec.prac.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -12,24 +15,28 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+@RequiredArgsConstructor
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
     private final JwtTokenProvider jwtTokenProvider;
-
-    public SecurityConfig(JwtTokenProvider jwtTokenProvider) {
-        this.jwtTokenProvider = jwtTokenProvider;
-    }
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+                .and()
                 .formLogin().disable()
                 .httpBasic().disable()
                 .cors().disable()
                 .csrf().disable()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
 
                 .and()
                 .authorizeRequests(auth -> auth
